e0068f78d003c49542f7c3c73bd50faa5e83d5b9
leigq <191789784@qq.com>
2019-03-14 16:32:28 +0800

    动态刷新zuul的配置，结合配置中心bus

--- src/main/java/com/leigq/www/ApiGatewayApplication.java
+++ src/main/java/com/leigq/www/ApiGatewayApplication.java
@@ -2,7 +2,10 @@ package com.leigq.www;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.netflix.zuul.filters.ZuulProperties;
 
 @SpringBootApplication
 @EnableZuulProxy
@@ -10,6 +13,23 @@ public class ApiGatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+
+    /**
+     * 动态刷新zuul的配置，结合配置中心bus
+     * <br>创建人： leiGQ
+     * <br>创建时间： 2019-03-14 16:19
+     * <p>
+     * 修改人： <br>
+     * 修改时间： <br>
+     * 修改备注： <br>
+     * </p>
+     * <br>
+     */
+    @ConfigurationProperties("zuul")
+    @RefreshScope
+    public ZuulProperties zuulProperties() {
+        return new ZuulProperties();
     }
 
 }
