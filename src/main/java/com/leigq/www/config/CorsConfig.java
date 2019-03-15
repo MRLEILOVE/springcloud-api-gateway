994600514c824f038212085de53c89f260f08eb9
leigq <191789784@qq.com>
2019-03-15 15:12:16 +0800

    添加zuul路由的跨域配置

---
+++ src/main/java/com/leigq/www/config/CorsConfig.java
@@ -0,0 +1,34 @@
+package com.leigq.www.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+/**
+ * zuul路由的跨域配置
+ * 参考：<a href='https://blog.csdn.net/moshowgame/article/details/80507696'>spring-cloud-zuul跨域问题解决</a>
+ * <p>
+ * 创建人：LeiGQ <br>
+ * 创建时间：2019-03-15 15:07 <br>
+ * <p>
+ * 修改人： <br>
+ * 修改时间： <br>
+ * 修改备注： <br>
+ * </p>
+ */
+@Configuration
+public class CorsConfig {
+    @Bean
+    public CorsFilter corsFilter() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        final CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        return new CorsFilter(source);
+    }
+}
